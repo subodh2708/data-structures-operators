17beb808257721284141aa59465a190fc8fc83fe
Subodh <subodhlst@gmail.com>
2025-08-20 05:54:26 +0530

    practice logical assignment operator

--- script.js
+++ script.js
@@ -64,6 +64,39 @@ const restaurant = {
     console.log(mainIngredients, otherIngredients);
   },
 };
+
+// Logical assignment Operators
+
+const rest1 = {
+  name: 'La pinozz',
+  // numGuests: 6,
+  numGuests: 0,
+};
+
+const rest2 = {
+  name: 'Soul Chef',
+  owner: 'Avantika Parmar',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+console.log(rest1);
+console.log(rest2);
+
+// rest1.owner = rest1.owner && '<ANONYMOUS>';
+// rest2.owner = rest2.owner && '<ANONYMOUS>';
+
+rest1.owner &&= '<ANONYMOUS>';
+rest2.owner &&= '<ANONYMOUS>';
+
+console.log(rest1);
+console.log(rest2);
+
+/*
 // Nulish coalesing Operator
 
 restaurant.numGuest = 0;
@@ -73,7 +106,7 @@ console.log(guest1);
 console.log(guest2);
 console.log(restaurant);
 
-/*
+
 //short circuiting(|| and &&)
 
 console.log(3 || 'subodh');
